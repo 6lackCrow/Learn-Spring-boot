df6036d31f6c49295ac4cc29a6b3e1c26e624846
crow <1871339825@qq.com>
2019-10-09 14:21:33 +0800

    init

---
+++ src/main/java/com/spbdemo/xxdemo/XxdemoApplication.java
@@ -0,0 +1,13 @@
+package com.spbdemo.xxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxdemoApplication.class, args);
+    }
+
+}
